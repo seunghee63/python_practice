17fc95fa25b4cba3db33cfa0c7d5734895c5ae5a
seunghee63 <0603yang@naver.com>
2019-04-03 03:15:08 +0900

    add stack using list & modify queue

--- web_practice_python/stack_list.py
+++ web_practice_python/stack_list.py
@@ -1,32 +1,31 @@
-queue_list = list()
-
+stack_list = list()
+top = -1
 
 #1) while문으로 숫자를 입력 받아 리스트에 저장(저장 순서는 스택, 큐에 따라서 다름)
 
 while True:
 
-    n_node = input("enqueue : ",)
+    n_node = input("insert : ")
 
     if n_node != "":
-        queue_list.append(int(n_node))
+        top +=1
+        stack_list.insert(top,int(n_node))
     else:
         break
 
-
-print("*queue : ", queue_list)
+print("input :", stack_list, "top :", top,)
 print()
 
 
 #2) 리스트의 크기만큼 while문을 돌려서 데이터 출력(각 자료구조의 특성에 맞게), 출력시에는 값을 출력함과 동시에 리스트에서 해당요소가 삭제되어야함.
 
-while True :
-    i=0;
+while True:
 
-    print("dequeue : ", queue_list[i])
-    queue_list.pop(i)
-
-    #마지막일 때
-    if(0==len(queue_list)):
+    if top >= 0:
+        print("pop :",stack_list[top])
+        stack_list.pop(top)
+        top-=1
+    else :
         break
 
-print("*queue :" , queue_list)
+print("result :", stack_list, "top :", top)
