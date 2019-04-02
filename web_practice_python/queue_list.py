89134582f8b4df537b7963d9625ff4517a65cc33
seunghee63 <0603yang@naver.com>
2019-04-03 01:33:18 +0900

    queue using list

---
+++ web_practice_python/queue_list.py
@@ -0,0 +1,31 @@
+queue_list = list()
+
+
+#1) while문으로 숫자를 입력 받아 리스트에 저장(저장 순서는 스택, 큐에 따라서 다름)
+
+while True:
+
+    n_node = input()
+
+    if n_node != "":
+        queue_list.append(int(n_node))
+    else:
+        break
+
+
+print("input :", queue_list)
+
+
+#2) 리스트의 크기만큼 while문을 돌려서 데이터 출력(각 자료구조의 특성에 맞게), 출력시에는 값을 출력함과 동시에 리스트에서 해당요소가 삭제되어야함.
+
+while True :
+    i=0;
+
+    print("삭제 원소 : ", queue_list[i])
+    queue_list.pop(i)
+
+    #마지막일 때
+    if(0==len(queue_list)):
+        break
+
+print("result :" , queue_list)
