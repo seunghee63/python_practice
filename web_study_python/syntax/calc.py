9e0a8c7943e8f2dfa060afb80ffed60993415b5d
seunghee63 <0603yang@naver.com>
2019-04-05 20:07:15 +0900

    2nd week

---
+++ web_study_python/syntax/calc.py
@@ -0,0 +1,11 @@
+def add(a,b):
+    return a+b
+
+def sub(a,b):
+    return a-b
+
+#name space가 main일 때만 실행
+if __name__ == "__main__":
+    print(add(1,3))
+    print(sub(1,3))
+
---
+++ web_study_python/upper.py
@@ -0,0 +1,7 @@
+
+myList =[“apple”, “banana”, “mango”,“mellon”]
+
+for each in myList:
+    
+    each = each.upper()
+    print(each)
